068826a96893cb1d71df564c129cc99c50943288
Rami Sabbagh <ramilego4game@gmail.com>
2020-08-19 16:39:23 +0300

    Update LÖVE's releases base URL

--- linux/download_appimages.lua
+++ linux/download_appimages.lua
@@ -9,12 +9,12 @@ dofile(GITHUB_WORKSPACE.."/lua_utils/shared.lua") --Load the shared utilities
 
 --LÖVE AppImages
 
-local baseURL = "https://bitbucket.org/rude/love/downloads/love-%s-%s.AppImage"
+local baseURL = "https://github.com/love2d/love/releases/download/%s/love-%s-%s.AppImage"
 local baseDestination = "love-%s-%s.AppImage"
 
 for _, platform in ipairs(LINUX_PLATFORMS) do
     print("Downloading LÖVE "..tostring(LOVE_VERSION).." ")
-    local url = string.format(baseURL, LOVE_VERSION, platform)
+    local url = string.format(baseURL, LOVE_VERSION, LOVE_VERSION, platform)
     local destination = string.format(baseDestination, LOVE_VERSION, platform)
     wget(url, destination)
     chmod(destination)
