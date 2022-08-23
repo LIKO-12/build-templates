5f68e3b8aadbcefe70bb0cb52ce5c1ac72f7f9b8
Rami Sabbagh <rami.sab07@gmail.com>
2022-08-23 23:45:26 +0300

    Fix love binaries fetching

--- windows/fetch_love.sh
+++ windows/fetch_love.sh
@@ -8,7 +8,7 @@ for arch in 'win32' 'win64'; do
     echo "Download love-${LOVE_VERSION}-${arch}.zip";
     echo "";
     
-    curl -o "build/love_${arch}.zip" "https://github.com/love2d/love/releases/download/${LOVE_VERSION}/love-${LOVE_VERSION}-${arch}.zip";
+    curl -o "build/love_${arch}.zip" -L "https://github.com/love2d/love/releases/download/${LOVE_VERSION}/love-${LOVE_VERSION}-${arch}.zip" --fail-with-body;
 done
 
 echo '';
