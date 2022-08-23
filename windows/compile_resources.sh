4022e224bd0887ccf483a0d13d629a6a13a81361
Rami Sabbagh <rami.sab07@gmail.com>
2022-08-23 23:04:00 +0300

    Compile PE resources

---
+++ windows/compile_resources.sh
@@ -0,0 +1,16 @@
+[[ -d 'build' ]] || mkdir 'build';
+[[ -d 'build/res' ]] || mkdir 'build/res';
+
+reshack='build/reshack/ResourceHacker.exe';
+
+for file in windows/resources/*.rc; do
+    filename=$(basename $file .rc);
+
+    echo '';
+    echo "========== Compile $filename ==========";
+
+    source="windows/resources/${filename}.rc";
+    destination="build/res/${filename}.res";
+
+    $reshack -open $source -save $destination -action compile -log CONSOLE;
+done
